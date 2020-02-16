5b2c6d1a73f4caf8a409bb8dea4828b696ce6b77
Di Zheng <dizhengcindy@gmail.com>
2020-02-16 06:54:47 +0000

    Done.

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -6,12 +6,39 @@ def join_ingredients(src)
   #
   # "I love (inner array element 0) and (inner array element 1) on my pizza""
   # As such, there should be a new String for each inner array, or pair
+  row =0 
+  newArray = []
+  while row < src.count do 
+    col = 1 
+    while col < src[row].count do
+      newArray << "I love #{src[row][col-1]} and #{src[row][col]} on my pizza"
+      col += 1
+    end
+    row += 1
+    end
+    newArray
 end
 
 def find_greater_pair(src)
   # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
   # Produce a new Array that contains the larger number of each of the pairs
   # that are in the inner Arrays
+  row = 0 
+  newArray = []
+  while row < src.count do 
+    col = 1 
+    while col < src[row].count do
+      if src[row][col-1] < src[row][col]
+        newArray << src[row][col]
+      else
+        newArray << src[row][col-1]
+      end
+      col +=1
+    end
+    row +=1
+  end
+  newArray
+  
 end
 
 def total_even_pairs(src)
@@ -22,4 +49,19 @@ def total_even_pairs(src)
   # As a reminder any number % 2 will return 0 or 1. If the result is 0, then
   # the number was even. Review the operator documentation if you've forgotten
   # this!
+  
+  row = 0 
+  total =0
+  while row < src.count do 
+    col = 1 
+    while col < src[row].count do
+     if src[row][col]%2 == 0 && src[row][col-1]%2 == 0
+       total += src[row][col] 
+       total += src[row][col-1]
+     end
+      col +=1
+    end
+    row +=1
+  end
+  total
 end
